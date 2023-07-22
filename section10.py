3c57a55bdca5dc07b7e6f15ae55176a4a77d8c3c
chamchi <44095761+3chamchi@users.noreply.github.com>
2023-07-22 11:42:34 +0900

    Create section10.py

---
+++ section10.py
@@ -0,0 +1,30 @@
+# Section 10
+# ChatGPT API 연동
+import requests
+
+# 1. API 연동을 위한 준비
+
+# 1-1. API 연동을 위한 요청 패키지 설치.  pip install requests
+# 1-2. ChatGPT API 키 가져오기
+chatgpt_api_key = "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"
+
+# 2. ChatGPT API 요청
+url = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {chatgpt_api_key}"
+}
+
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "user",
+            "content": "반가워 chatGPT"
+        }
+    ]
+}
+
+response = requests.post(url, headers=headers, json=data)
+print(response.json())
