e0588c40247bff4e648b9ea3e16a5a0a68a47a5c
chamchi <3chamchi@gmail.com>
2023-08-12 12:16:15 +0900

    (수정)8강 오디오 파일 업로드. - 윈도우 파일 경로 추가 - Audio 기존 텍스트 변환 + 영어 텍스트 변환 코드 추가

--- section16.py
+++ section16.py
@@ -4,16 +4,18 @@
 # 1. API 연동 준비
 import openai
 
-openai.api_key = "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"
+openai.api_key = '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'
 
 # 2. 오디오 파일 읽기
-# file_name = '애국가 제창(1절).mp3'
-file_name = '짧은 대화의 응답_22006-0337_통파일.mp3'  # 파일 이름 변수, 확장자 포함
-audio_file = open(f'media/{file_name}', 'rb')  # 파일 열기
+# 파일 경로
+#  - Windows : open(r'media\짧은 대화의 응답_22006-0337_통파일.mp3', 'rb')
+#  - Mac : open('media/짧은 대화의 응답_22006-0337_통파일.mp3', 'rb')
+audio_file = open('media/짧은 대화의 응답_22006-0337_통파일.mp3', 'rb')
 
 # 3. API 요청
-response = openai.Audio.transcribe("whisper-1", audio_file)  # API 요청
+response = openai.Audio.transcribe('whisper-1', audio_file)  # 음성 파일의 대화를 해당 언어의 텍스트로 변환
+# response = openai.Audio.translate('whisper-1', audio_file)  # 음성 파일의 대화를 영어 텍스트로 변환
 
-# 4. API 요청 응답 확인
+# 4. API 응답 확인
 print(response)
 print(response['text'])
